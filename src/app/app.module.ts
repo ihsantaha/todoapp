8f385a6aedc54e5d19003906de2c18949d59b2a7
Ihsan Taha <ihsan_taha@yahoo.com>
2019-02-10 22:05:08 -0500

    Update on todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { ErrorComponent } from './error/error.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    LogoutComponent,
+    HomeComponent,
+    WelcomeComponent,
+    TodoListComponent,
+    FooterComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
